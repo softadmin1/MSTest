ac04b36ed08ddf1c9db0f890a2fe2ded64d05ac5
softcloudhub <iotcloudsa@gmail.com>
2018-12-31 18:11:31 +0300

    first commit

---
+++ src/main/java/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan({"controller","service"})
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/service/PersonService.java
@@ -0,0 +1,60 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.stream.Stream;
+
+import org.springframework.stereotype.Service;
+
+import model.Person;
+
+@Service
+public class PersonService {
+	ArrayList<Person> persons = new ArrayList<Person>();
+	public PersonService() {
+		Person p = new Person();
+		p.setId("1");
+		p.setAge(21);
+		p.setFirstName("Steve");
+		p.setLastName("Smith");
+		persons.add(p);
+
+		p = new Person();
+		p.setId("2");
+		p.setAge(25);
+		p.setFirstName("Tom");
+		p.setLastName("Brown");
+		persons.add(p);
+		
+		p = new Person();
+		p.setId("3");
+		p.setAge(33);
+		p.setFirstName("ttttt");
+		p.setLastName("rrrrr");
+		persons.add(p);
+		
+		p = new Person();
+		p.setId("4");
+		p.setAge(25);
+		p.setFirstName("fffffff");
+		p.setLastName("yyyyyy");
+		persons.add(p);
+		
+		p = new Person();
+		p.setId("5");
+		p.setAge(25);
+		p.setFirstName("rrrrrr");
+		p.setLastName("uuuuuu");
+		persons.add(p);
+	}
+	public Person getPerson(String id) {
+		for(Person person:persons) {
+			if(person.getId().equalsIgnoreCase(id)) return person;
+		}
+	    return null;
+	}
+	public ArrayList<Person> getAll() {
+		return persons;
+	}
+}
+
