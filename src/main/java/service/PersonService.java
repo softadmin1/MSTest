71df1e33c9a2d13d1cd0f0c8d3759c4091c9782d
softcloudhub <iotcloudsa@gmail.com>
2019-01-10 09:58:02 +0300

    first commit

--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -39,6 +39,13 @@ public class PersonService {
 		p.setFirstName("saud");
 		p.setLastName("al arifi");
 		persons.add(p);
+		
+		p = new Person();
+		p.setId("5");
+		p.setAge(25);
+		p.setFirstName("ismael");
+		p.setLastName("mohamed");
+		persons.add(p);
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
